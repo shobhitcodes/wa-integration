f662d6bd89626dea2b1ac108abb1f41555cea31f
shobhitcodes <mailshobhitnow@gmail.com>
2024-07-10 00:52:21 +0530

    Update app.js

--- app.js
+++ app.js
@@ -132,38 +132,34 @@ app.post('/webhook', (req, res) => {
 
 const sendMessage = async (phoneNumber, message) => {
   try {
-    const response = await axios.post(
-      process.env.WHATSAPP_API_URL,
-      {
-        // messaging_product: 'whatsapp',
-        // recipient_type: 'individual',
-        // to: phoneNumber,
-        // type: 'text',
-        // text: { body: message },
-        // "messaging_product": "whatsapp",
-        // "recipient_type": "individual",
-        // "to": "+16505555555",
-        // "type": "text",
-        // "text": {
-        //   "preview_url": true,
-        //   "body": "Here'\''s the info you requested! https://www.meta.com/quest/quest-3/"
-        // }
-        messaging_product: 'whatsapp',
-        to: phoneNumber,
-        type: 'text',
-        text: {
-          preview_url: false,
-          body: 'check101',
-        },
-      },
-      {
-        headers: {
-          Authorization: `Bearer ${process.env.WHATSAPP_TOKEN}`,
-          'Content-Type': 'application/json',
-        },
-      }
-    );
-    console.log('Message sent:', response.data);
+// Define the URL and headers
+const url = 'https://graph.facebook.com/v20.0/405781202600002/messages';
+const headers = {
+  'Authorization': 'Bearer EAAEw3iFNUckBO2VL0YoYcChgI6b5TpTYUwZBSZCHPArlOvNxE0ogAgNuz2eneCbZCfoHyYmWvyReo32Eesj0CPnyHQ20HCFJCwvZBDWoEvgjEVVdmFdFBD0FNFq4x547g5ewltzp0NGNqW48lr3lYxwQHt9VnhylptorPteHz7B9VY4lMwnLhNyG8vvOqa576fDKKG4sE2TMTa4Rz0ukC9mPnl4ZD',
+  'Content-Type': 'application/json',
+};
+
+// Define the request body
+const data = {
+  messaging_product: 'whatsapp',
+  to: '918929999699',
+  type: 'template',
+  template: {
+    name: 'hello_world',
+    language: {
+      code: 'en_US'
+    }
+  }
+};
+
+// Send the POST request
+axios.post(url, data, { headers })
+  .then(response => {
+    console.log('Response:', response.data);
+  })
+  .catch(error => {
+    console.error('Error:', error.response ? error.response.data : error.message);
+  });
   } catch (error) {
     console.error(
       'Error sending message:',
